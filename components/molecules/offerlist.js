18dc03ec875f32a3fbbd89e038de90e1c234969c
Madoxen <pchlad5@gmail.com>
2021-06-10 20:22:06 +0200

    Add offer and your offers now works

--- components/molecules/offerlist.js
+++ components/molecules/offerlist.js
@@ -3,22 +3,22 @@ import { List } from "native-base";
 import OfferListBox from "../atoms/offerlistbox";
 
 export default OfferList = ({ list, onClick }) => {
-  const offerList = list.map((user) => (
+
+  const offerList = list.map((offer) => (
     <OfferListBox
-      key={user.id}
-      title={user.title}
-      company={user.company}
-      payment={user.payment}
-      location={user.location}
-      url={user.url}
-      time={user.time}
+      key={offer.id}
+      title={offer.position}
+      company={offer.company}
+      payment={offer.salary}
+      location={offer.place}
+      url={offer.url}
+      time={offer.date?.toDate().toString()}
       onClick={onClick}
     />
   ));
 
   return (
     <List>
-      {offerList}
       {offerList}
     </List>
   );
--- scenes/myofferts.js
+++ scenes/myofferts.js
@@ -1,81 +1,48 @@
-import { Button,Header, Container, Content, Icon, Text, View } from 'native-base'
+import { Button, Header, Container, Content, Icon, Text, View } from 'native-base'
 import { StyleSheet } from 'react-native'
 import React from 'react'
 import TopBar from '../components/atoms/topbar'
 import Btn from '../components/atoms/btn'
 import Offerlist from '../components/molecules/offerlist'
+import { useEffect } from 'react'
+import * as firebase from 'firebase'
+import { useState } from 'react'
+
 
 export default MyOfferts = ({ navigation }) => {
-    
-    const companyList = [
-        {
-          id: 0,
-          title: "Java Developer",
-          location: "Berlin",
-          payment: "5000 - 6000 EUR",
-          company: "Ubisoft",
-          url: "https://www.gry-online.pl/galeria/kontakty/344743037.png",
-        },
-        {
-          id: 1,
-          title: "Python Developer",
-          location: "Paris",
-          payment: "5000 - 8000 EUR",
-          company: "Microsoft",
-          url: "https://cloudwize.co.za/wp-content/uploads/2019/03/new-microsoft-logo-SIZED-SQUARE.jpg",
-        },
-        {
-          id: 2,
-          title: "Front End Dev",
-          location: "New York",
-          payment: "9000 - 14000 USD",
-          company: "Google",
-          url: "https://bookassist.org/wp-content/uploads/elementor/thumbs/google_3_520-oc7dqerwmsbfad0t1gveosa6x2uck2bd7y6l2r7txs.jpg",
-        },
-        {
-          id: 3,
-          title: "Back End Dev",
-          location: "California",
-          payment: "8000 - 11000 USD",
-          company: "Facebook",
-          url: "https://i.pinimg.com/736x/ac/57/3b/ac573b439cde3dec8ca1c6739ae7f628.jpg",
-        },
-        {
-          id: 4,
-          title: "Student",
-          location: "Nigeria",
-          payment: "100 - 200 USD",
-          company: "Amazon",
-          url: "https://s.clipartkey.com/mpngs/s/147-1478713_amazon-logo-png-amazon-logo.png",
-        },
-      ];
 
-    const onClickGoBack = () => {
-        navigation.goBack();
-    }
+  const [offerList, setOfferList] = useState([])
 
-    const onClickAdd = () => {
-        navigation.navigate("EditOffert");
-    }
+  useEffect(() => {
+    const uid = firebase.auth().currentUser.uid
+    const offersRef = firebase.firestore().collection("offers")
+    offersRef.where("ownerUid", "==", uid).get().then(snap => {
+      const arr = snap.docs.map(x => x.data())
+      setOfferList(arr)
+    })
+  }, [])
 
-    return (
-        <Container>
-            <Header>
-                <TopBar title="My Offerts" onClickGoBack={onClickGoBack}/>
-            </Header>
-            <Content>
-                <Offerlist list={companyList} />
-                <View style={styles.btnView}>
-                    <Btn onPress={onClickAdd} icon={"add"} text={"Add New"}/>
-                </View>
-            </Content>
-        </Container>
-    )
+
+  const onClickGoBack = () => {
+    navigation.goBack();
+  }
+
+
+  return (
+    <Container>
+      <Header>
+        <TopBar title="My Offerts" onClickGoBack={onClickGoBack} />
+      </Header>
+      <Content>
+        <Offerlist list={offerList} />
+      </Content>
+    </Container>
+  )
 }
 
 const styles = StyleSheet.create({
-    btnView: {
-        paddingHorizontal: 16,
-        paddingVertical: 20,
-    }
+  btnView: {
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+  }
 })
